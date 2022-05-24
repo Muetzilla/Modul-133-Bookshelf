4084ee95c2a591756da9c4744b4f5f4ddaa884d7
Muetzilla <muetz-github@gasbichler.ch>
2022-05-24 10:32:26 +0200

    CRUD for Book working

--- Bookshelf/src/main/java/ch/bzz/bookshelf/service/BookService.java
+++ Bookshelf/src/main/java/ch/bzz/bookshelf/service/BookService.java
@@ -1,15 +1,14 @@
 package ch.bzz.bookshelf.service;
 
 import ch.bzz.bookshelf.model.Book;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import ch.bzz.bookshelf.data.DataHandler;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * services for reading, adding, changing and deleting books
@@ -51,6 +50,80 @@ public class BookService {
         return Response
                 .status(httpStatus)
                 .entity(book)
+                .build();
+    }
+
+
+    @GET
+    @Path("delete")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteBook(
+            @QueryParam("uuid") String bookUUID
+    ) {
+        int httpStatus = 200;
+        DataHandler.getInstance().deleteBook(bookUUID);
+        return Response
+                .status(httpStatus)
+                .build();
+    }
+
+    @PUT
+    @Path("update")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response updateBook(
+            @FormParam("bookUUID") String bookUUID,
+            @FormParam("title") String title,
+            @FormParam("author") String author,
+            @FormParam("publisherUUID") String publisherUUID,
+            @FormParam("price") BigDecimal price,
+            @FormParam("isbn") String isbn    ) {
+        int httpStatus = 200;
+        Book book = DataHandler.getInstance().readBookByUUID(bookUUID);
+        book.setTitle(title);
+        book.setAuthor(author);
+        book.setPublisherUUID(publisherUUID);
+        book.setPrice(price);
+        book.setIsbn(isbn);
+
+        DataHandler.getInstance().updateBook();
+        return Response
+                .status(httpStatus)
+                .build();
+    }
+
+    /**
+     *
+     * @param title
+     * @param author
+     * @param publisherUUID
+     * @param price
+     * @param isbn
+     * @return
+     */
+    @POST
+    @Path("create")
+    @Produces(MediaType.TEXT_PLAIN)
+
+    public Response insertBook(
+            @FormParam("title") String title,
+            @FormParam("author") String author,
+            @FormParam("publisherUUID") String publisherUUID,
+            @FormParam("price") BigDecimal price,
+            @FormParam("isbn") String isbn    ) {
+        Book book = new Book();
+        book.setBookUUID(String.valueOf(UUID.randomUUID()));
+        book.setTitle(title);
+        book.setAuthor(author);
+        book.setPublisherUUID(publisherUUID);
+        book.setPrice(price);
+        book.setIsbn(isbn);
+
+        DataHandler.getInstance().insertBook(book);
+
+        int httpStatus = 200;
+
+        return Response
+                .status(httpStatus)
                 .build();
     }
 }
